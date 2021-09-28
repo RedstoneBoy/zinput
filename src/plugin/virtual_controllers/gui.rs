2f1f008f167b7c53f7083072489d0d3abe44f972
Shoaib Syed <shoaibmsyed@gmail.com>
2021-09-27 23:19:39 -0400

    vcontroller inputs

--- src/plugin/virtual_controllers/gui.rs
+++ src/plugin/virtual_controllers/gui.rs
@@ -5,9 +5,9 @@ use eframe::{egui, epi};
 use crate::api::component::COMPONENT_KINDS;
 use crate::zinput::engine::Engine;
 
-use super::MAX_CONTROLLERS;
 use super::state::State;
 use super::vcontroller::VInput;
+use super::MAX_CONTROLLERS;
 
 const ROW_WIDTH: usize = 4;
 
@@ -27,12 +27,24 @@ impl Gui {
             edit: EditContext::empty(),
         }
     }
-    
-    pub fn update(&mut self, state: &mut State, ctx: &egui::CtxRef, frame: &mut epi::Frame, ui: &mut egui::Ui) {
+
+    pub fn update(
+        &mut self,
+        state: &mut State,
+        ctx: &egui::CtxRef,
+        frame: &mut epi::Frame,
+        ui: &mut egui::Ui,
+    ) {
         self.update_main_window(state, ctx, frame, ui);
     }
 
-    fn update_main_window(&mut self, state: &mut State, ctx: &egui::CtxRef, frame: &mut epi::Frame, ui: &mut egui::Ui) {
+    fn update_main_window(
+        &mut self,
+        state: &mut State,
+        ctx: &egui::CtxRef,
+        frame: &mut epi::Frame,
+        ui: &mut egui::Ui,
+    ) {
         if self.editing {
             self.update_edit_window(state, ctx, frame, ui);
             return;
@@ -42,44 +54,112 @@ impl Gui {
             .min_col_width(125.0)
             .min_row_height(50.0)
             .show(ui, |ui| {
-            let mut x = 0;
-            for i in 0..MAX_CONTROLLERS {
-                if x >= ROW_WIDTH {
-                    x = 0;
-                    ui.end_row();
-                }
-
-                x += 1;
-
-                ui.vertical_centered(|ui| {
-                    ui.label(format!("Virtual Controller {}", i + 1));
-
-                    if i < state.vcons.len() {
-                        if ui.button(state.vcons[i].name()).clicked() {
-                            // todo
-                        }
-                    } else if i == state.vcons.len() && state.vcons.len() < MAX_CONTROLLERS {
-                        if ui.button("Add Controller").clicked() {
-                            self.edit = EditContext::add_controller();
-                            self.editing = true;
-                        }
-                    } else if i > state.vcons.len() {
-                        ui.label("");
+                let mut x = 0;
+                for i in 0..MAX_CONTROLLERS {
+                    if x >= ROW_WIDTH {
+                        x = 0;
+                        ui.end_row();
                     }
-                });
-            }
-        });
+
+                    x += 1;
+
+                    ui.vertical_centered(|ui| {
+                        ui.label(format!("Virtual Controller {}", i + 1));
+
+                        if i < state.vcons.len() {
+                            if ui.button(state.vcons[i].name()).clicked() {
+                                // todo
+                            }
+                        } else if i == state.vcons.len() && state.vcons.len() < MAX_CONTROLLERS {
+                            if ui.button("Add Controller").clicked() {
+                                self.edit = EditContext::add_controller();
+                                self.editing = true;
+                            }
+                        } else if i > state.vcons.len() {
+                            ui.label("");
+                        }
+                    });
+                }
+            });
     }
 
-    fn update_edit_window(&mut self, state: &mut State, ctx: &egui::CtxRef, frame: &mut epi::Frame, ui: &mut egui::Ui) {
+    fn update_edit_window(
+        &mut self,
+        state: &mut State,
+        ctx: &egui::CtxRef,
+        frame: &mut epi::Frame,
+        ui: &mut egui::Ui,
+    ) {
         // Profile
 
         // Inputs
         ui.heading("Input Devices");
         egui::Grid::new("vcinputgrid").show(ui, |ui| {
-            // for device_id
+            let mut x = 0;
+            let mut to_remove = None;
+            for i in 0..self.edit.input.devices.len() {
+                let cur_device = self.edit.input.devices[i];
+                egui::ComboBox::from_label(format!("Device {}", i + 1))
+                    .selected_text(
+                        state
+                            .engine
+                            .get_device(&cur_device)
+                            .map_or("".to_owned(), |dev| dev.name.clone()),
+                    )
+                    .show_ui(ui, |ui| {
+                        for device in state.engine.devices() {
+                            ui.selectable_value(
+                                &mut self.edit.input.devices[0],
+                                *device.key(),
+                                state
+                                    .engine
+                                    .get_device(device.key())
+                                    .map_or("".to_owned(), |dev| dev.name.clone()),
+                            );
+                        }
+                        let mut remove = false;
+                        ui.selectable_value(
+                            &mut remove,
+                            true,
+                            "[Remove]"
+                        );
+                        if remove {
+                            to_remove = Some(i);
+                        }
+                    });
+                    
+                x += 1;
+                if x >= 4 {
+                    ui.end_row();
+                    x = 0;
+                }
+            }
+
+            if let Some(to_remove) = to_remove {
+                self.edit.input.devices.remove(to_remove);
+            }
+
+            let mut selected = None;
+
+            egui::ComboBox::from_label("New Device")
+                .show_ui(ui, |ui| {
+                    for device in state.engine.devices() {
+                        ui.selectable_value(
+                            &mut selected,
+                            Some(*device.key()),
+                            state
+                                .engine
+                                .get_device(device.key())
+                                .map_or("".to_owned(), |dev| dev.name.clone()),
+                        );
+                    }
+                });
+            
+            if let Some(new_device) = selected {
+                self.edit.input.devices.push(new_device);
+            }
         });
-        
+
         // Save / Close buttons
         ui.separator();
         ui.horizontal(|ui| {
@@ -127,12 +207,10 @@ enum EditMode {
     Add,
 }
 
-struct VCProfile {
-
-}
+struct VCProfile {}
 
 impl VCProfile {
     fn new() -> Self {
-        VCProfile {  }
+        VCProfile {}
     }
 }
